cc011084e8d89c18dc86e25572c6572fa606ff69
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-10-13 21:40:30 +0200

    Enforce HTTPS and clean up and simplify styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import './App.css';
 import React from 'react';
 
 export default function App() {
+  const apps = ['pomodoro'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>async-await.net</h1>
-      </header>
-      <p>Welcome to async-await.net!</p>
-      <ul>
-        <li>
-          <a href="http://pomodoro.async-await.net">Pomodoro</a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <h1>async-await.net</h1>
+      <p>Welcome to async-await.net! Check out the apps below:</p>
+      {apps.map(app => (
+        <a key={app} href={`https://${app}.async-await.net`}>
+          <img alt="" src={`https://${app}.async-await.net/favicon.ico`} />
+          Pomodoro
+        </a>
+      ))}
+    </>
   );
 };
