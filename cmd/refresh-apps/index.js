c2f5cd98a14fc8e4629055f4c33eec2a41415655
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-12-08 11:45:58 +0100

    Use the page and limit hooks to fetch with the PAT

--- cmd/refresh-apps/index.js
+++ cmd/refresh-apps/index.js
@@ -1,15 +1,24 @@
 const GitHub = require('github-api');
+const fs = require('fs-extra');
 
 // Crash on an unhandled promise rejection
 process.on('unhandledRejection', error => { throw error; });
 
 void async function () {
   let apps = [];
-  for await (const repo of GitHub.getUsersUserRepos(process.argv[2], 'TomasHubelbauer')) {
+  for await (const repo of GitHub.getUsersUserRepos('TomasHubelbauer', { token: process.argv[2], onPageChange, onLimitChange })) {
     if (repo.topics.includes('async-await-net')) {
       apps.push({ name: repo.name, homepage: repo.homepage });
     }
   }
 
-  console.log(JSON.stringify(apps, null, 2));
+  await fs.writeJson('public/apps.json', apps, { spaces: 2 });
 }()
+
+function onPageChange({ page, total, url, attempt }) {
+  console.log(`${page}/${total || 'unknown'} of ${url}, attempt #${attempt}`);
+}
+
+function onLimitChange({ remaining, limit, reset }) {
+  console.log(`${remaining}/${limit} resetting at ${reset.toLocaleString()}`);
+}
